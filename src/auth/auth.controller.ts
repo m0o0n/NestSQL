1c0c5744acea784f1e9b2249889ca13fde98cdc7
Vlad Shtrikun <vlad.shtrikun@bndigital.co>
2025-04-17 16:53:08 +0300

    Add cookies authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,34 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {
+  constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) res: Response) {
+    try {
+      const token = await this.authService.login(loginDto);
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+        maxAge: 60 * 60 * 1000, // 1 hour
+      });
+      return "Success"
+    } catch (error) {
+      throw new UnauthorizedException('Invalid credentials');
     }
-
-    @HttpCode(HttpStatus.OK)
-    @Post('login')
-    login(@Body() loginDto: LoginDto) {
-        return this.authService.login(loginDto);
-    }
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,13 +14,13 @@ export class AuthService {
     const user = await this.UsersService.findOneByEmail(email);
     console.log(user[0].password, password);
     if (user[0].password !== password) {
+      console.log('Invalid credentials');
       throw new UnauthorizedException();
+      
     }
 
     const payload = { user_id: user.id };
 
-    return {
-      jwt: this.jwt.sign(payload),
-    };
+    return this.jwt.sign(payload);
   }
 }
