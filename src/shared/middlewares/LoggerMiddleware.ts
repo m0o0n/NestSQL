f483961458b0edc3bd0521888ce31707400b5c5a
Vlad Shtrikun <vlad.shtrikun@bndigital.co>
2025-04-17 17:29:06 +0300

    Add next for LoggerMiddleware

--- src/shared/middlewares/LoggerMiddleware.ts
+++ src/shared/middlewares/LoggerMiddleware.ts
@@ -5,5 +5,6 @@ import { Request, Response, NextFunction } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log(`[${req.method}] at ${req.path} status: ${res.statusCode}`);
+    next();
   }
 }
